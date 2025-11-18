9509517a814921a19694aae9e7cf9711a8920009
Taki-glitch <tsqdevin@gmail.com>
2025-11-18 13:41:04 +0100

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-// script.js — Point d'entrée principal (module ES6)
-import { initApp } from "./modules/app.js";
-
-console.log("✅ script.js chargé en mode module !");
-document.addEventListener("DOMContentLoaded", initApp);
